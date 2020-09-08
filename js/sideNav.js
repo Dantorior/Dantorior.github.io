c3bb8be66fd888b4fc57cdba4024cc1ba6ecb47d
Dmitriy <59513717+Dantorior@users.noreply.github.com>
2020-09-08 20:57:00 +0300

    deploy v2

---
+++ js/sideNav.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("main" && "footer").style.marginLeft = "250px"; document.getElementById("main" && "footer").style.minWidth = "100%";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "";
+  document.getElementById("main" && "footer").style.marginLeft = "";
+  document.getElementById("main" && "footer").style.minWidth = "";
+}

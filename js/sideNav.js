cff4ed7b08554c224f56abf59243711109efa3e7
Dmitriy <59513717+Dantorior@users.noreply.github.com>
2020-09-12 14:03:17 +0300

    3

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,10 +1,15 @@
 function openNav() {
   document.getElementById("sideNav").style.width = "250px";
-  document.getElementById("main" && "footer").style.marginLeft = "250px"; document.getElementById("main" && "footer").style.minWidth = "100%";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("footer").style.marginLeft = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("footer").style.minWidth = "100%";
 }
 
 function closeNav() {
   document.getElementById("sideNav").style.width = "";
-  document.getElementById("main" && "footer").style.marginLeft = "";
-  document.getElementById("main" && "footer").style.minWidth = "";
+  document.getElementById("main").style.marginLeft = "";
+  document.getElementById("footer").style.marginLeft = "";
+  document.getElementById("main").style.marginLeft = "";
+  document.getElementById("footer").style.minWidth = "";
 }
